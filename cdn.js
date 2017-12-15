6dc9a77e06830c2f7fc30bf5ed3e9e15a54c6157
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-15 10:30:25 +0200

    beep

--- cdn.js
+++ cdn.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const fileUpload = require("express-fileupload");
-fs.mkdirSync("data");
+try {fs.mkdirSync("data");}catch(error){}
 app.get("/", (req, res)=>{
     res.send("hello!");
 });
