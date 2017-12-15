d4a546aee9796874df570a10271c898a45ad989b
RONTheCookie <ronthecookie0101@gmail.com>
2017-12-15 10:22:57 +0200

    setup basic app, testing to see what ip vpn shows with some weird setup

---
+++ cdn.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const fileUpload = require("express-fileupload");
+fs.mkdirSync("data");
+app.get("/", (req, res)=>{
+    console.log(req.ip);
+});
+app.use(fileUpload({safeFileNames:true, preserveExtension:true}));
